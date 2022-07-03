244ccc2ec06d5d236416c53b513f2c2102d7367c
Muhammadsami24 <98199059+Muhammadsami24@users.noreply.github.com>
2022-07-03 14:27:07 +0500

    change

--- scr/screen/Home.js
+++ scr/screen/Home.js
@@ -1,26 +1,49 @@
-import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Image, TextInput } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
     <ScrollView>
-    <Image
+      <Image
         source={require("../../assets/Images/Image2.jpg")}
-        style={{ width: "100%", height: 400,}}
-        resizeMode="cover">
-          
+        style={styles.image}
+        resizeMode="cover"
+      >
+
       </Image>
-    
+      <View>
+        <TextInput
+          style={styles.input}
+        >
+
+        </TextInput>
+
+          <Image
+          source={require("../../assets/Images/Image3")}
+          >
+
+          </Image>
+     
+      </View>
+
     </ScrollView>
   )
 }
 const styles = StyleSheet.create({
-  container:{
-    paddingHorizontal:50,
-    paddingVertical:60,
-    margintop:100,
-    backgroundColor:"black",
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  input: {
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    marginLeft: 20,
+    marginRight: 80,
+    borderRadius: 40,
+    backgroundColor: "#23D5C6",
   }
+
 })
 
 export default Home
