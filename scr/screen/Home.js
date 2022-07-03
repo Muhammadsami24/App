03051f101c74fb1bd15e6e2603d89453d4ca33c9
Muhammadsami24 <98199059+Muhammadsami24@users.noreply.github.com>
2022-07-03 18:23:28 +0500

    commit

--- scr/screen/Home.js
+++ scr/screen/Home.js
@@ -1,43 +1,53 @@
-import { ScrollView, StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Image, TextInput,Searchbar } from 'react-native'
 import React from 'react'
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
   return (
     <ScrollView>
       <Image
         source={require("../../assets/Images/Image2.jpg")}
-        style={styles.image}
+        style={styles.image1}
         resizeMode="cover"
       >
 
       </Image>
-      <View>
-        <TextInput
-          style={styles.input}
-        >
-
-        </TextInput>
-
-          <Image
-          source={require("../../assets/Images/Image3")}
-          >
-
-          </Image>
-     
-      </View>
+      <Searchbar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
 
     </ScrollView>
   )
 }
 const styles = StyleSheet.create({
-  image: {
+  image1: {
     width: "100%",
     height: 300,
   },
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  image2: {
+    backgroundColor: "#23D5C6",
+    marginTop: 10,
+    marginRight: 20,
+    marginLeft: 5,
+    borderRadius: 40,
+    paddingTop: 20,
+    width: 24,
+    height: 24,
+    borderRadius: 20,
+  },
+ 
   input: {
     marginTop: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 30,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
     marginLeft: 20,
     marginRight: 80,
     borderRadius: 40,
